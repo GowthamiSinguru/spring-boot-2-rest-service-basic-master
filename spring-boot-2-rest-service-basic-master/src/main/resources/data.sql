c881b256f36a73845e11485f6e32c02673cb0389
GowthamiSinguru <gowthamisinguru14@gmail.com>
2018-02-28 16:31:47 -0600

    Initial commit

---
+++ spring-boot-2-rest-service-basic-master/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+
+
+insert into user  values ('1','anka','john','c','d','23','male');
+insert into user values('2','bobby','cena','e','f','23','female');
+
+insert into communication values('100','hari@gmail.com','513-884-5012');
+
+insert into communication values('101','charita@gmail.com','912-884-5012');
